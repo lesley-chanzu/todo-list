da967a4a4efeb8a253a6a6afc76211106eab6fee
Lesley Chanzu <lesleychanzu3@gmail.com>
2025-01-14 00:08:26 +0300

    list, delete and add tasks

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,11 +1,28 @@
 
 import { useState } from "react";
 const ToDoList = () => {
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  function addTask(task) {
+    setTasks([...tasks, task]);
+    setNewTask("");
+  }
+
+  function deleteTask(task) {
+    setTasks(tasks.filter(t => t !== task));
+  }
 
   return (
-    <div>ToDoList</div>
+    <>
+      <div>ToDoLists</div>
+      {tasks.map((task, index) => <div key={task}>
+        <p>{index + 1}. {task}</p>
+        <button onClick={() => deleteTask(task)}>Delete</button>
+      </div>)}
+      <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
+      <button onClick={() => addTask(newTask)}>Add Task</button>
+    </>
   )
 }
 
