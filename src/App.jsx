70f0ccde4d47ff0a561ad8cc125a7eba5c7ac784
Lesley Chanzu <lesleychanzu3@gmail.com>
2025-01-13 23:35:18 +0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ToDoList from "./components/ToDoList"
+import "./components/ToDoList.css"
+
+const App = () => {
+  return (
+    <div>
+      <ToDoList />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,12 @@
+
+import { useState } from "react";
+const ToDoList = () => {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+  return (
+    <div>ToDoList</div>
+  )
+}
+
+export default ToDoList
