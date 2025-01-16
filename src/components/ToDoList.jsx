7584c8f119336dc69714476cba731acb13e20ca0
Lesley Chanzu <lesleychanzu3@gmail.com>
2025-01-17 01:36:16 +0300

    edit functionality

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,9 +2,9 @@ import "./ToDoList.css";
 import { useState } from "react";
 const ToDoList = () => {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState("");
   const [editTask, setEditTask] = useState(null);
-  const [editedTask, setEditedTask] = useState('');
+  const [taskToEdit, setTaskToEdit] = useState("");
 
   function addTask(task) {
     setTasks([...tasks, { id: tasks.length + 1, title: task }]);
@@ -13,24 +13,84 @@ const ToDoList = () => {
   }
 
   function deleteTask(taskId) {
-    setTasks(tasks.filter(t => t.id !== taskId));
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
+
+  function updateTask() {
+    const newTasks = tasks.map((t) => {
+      if (t.id === editTask.id) {
+        return {
+          ...t,
+          title: taskToEdit
+        }
+      }
+      return t;
+    })
+    setTasks(newTasks);
+    setEditTask(null)
+    setTaskToEdit("")
   }
 
   return (
     <div className="container">
       <div className="todolists-app">
         <div className="header">ToDo List</div>
-        <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="Add new task" />
-        <button className="btn add-btn" onClick={() => addTask(newTask)}>Add Task</button>
-        {tasks.map((task, index) => <div key={task.id}>
-          {task.id === editTask?.id ? <input type="text" placeholder="edit task" /> :
-            <p>{index + 1}. {task.title}</p>}
-          <button className="btn edit-btn" onClick={() => setEditTask(task)}>Edit</button>
-          <button className="btn del-btn" onClick={() => deleteTask(task.id)}>Delete</button>
-        </div>)}
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add new task"
+        />
+        <button className="btn add-btn" onClick={() => addTask(newTask)}>
+          Add Your Task Here
+        </button>
+        {tasks.map((task, index) => (
+          <div key={task.id}>
+            {task.id === editTask?.id ? (
+              <input type="text" placeholder="edit task"
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+              />
+            ) : (
+              <p>
+                {index + 1}. {task.title}
+              </p>
+            )}
+            {task.id === editTask?.id ? (
+              <div>
+                {" "}
+                <button className="btn edit-btn" onClick={() => { updateTask() }}>
+                  Save
+                </button>
+                <button
+                  className="btn edit-btn"
+                  onClick={() => {
+                    setEditTask(null)
+                    setTaskToEdit("")
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
+            ) : (
+              <button
+                className="btn edit-btn"
+                onClick={() => {
+                  setEditTask(task);
+                  setTaskToEdit(task.title);
+                }}
+              >
+                Edit
+              </button>
+            )}
+            <button className="btn del-btn" onClick={() => deleteTask(task.id)}>
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default ToDoList
+export default ToDoList;
